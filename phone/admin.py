e73722e23efa323949b4658ed94f8bf026920ae2
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-10-03 09:58:04 -0300

    Register phone in admin.

--- phone/admin.py
+++ phone/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Phone
 
-# Register your models here.
+
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['phone_number', 'phone_type', 'department', 'sponsor', 'active', 'created_at', 'updated_at']
+    search_fields = ['phone_number', 'phone_type', 'department', 'sponsor']
+
+admin.site.register(Phone, PhoneAdmin)
