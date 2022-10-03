223718431218cbe5474409dd9002277b69495f1e
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-10-03 11:04:10 -0300

    Include phone, control and login url.

--- phone_lines/urls.py
+++ phone_lines/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView
+from phone import urls as phones_urls
+from control import urls as control_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(control_urls)),
+    path('phones/', include(phones_urls)),    
+    path('login/', LoginView.as_view(), name='login'),
 ]
