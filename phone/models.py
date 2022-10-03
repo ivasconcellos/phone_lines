6f407992876a3bca0c54abb32f3a923506b7e536
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-10-03 09:57:52 -0300

    Configured phone models.

--- phone/models.py
+++ phone/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class PhoneType(models.TextChoices):
+    ANALOG = 'Analógico', 'Analógico'
+    DIGITAL = 'Digital', 'Digital'
+
+class Phone(models.Model):
+    phone_number = models.CharField(max_length=4)
+    phone_type = models.CharField(max_length=9, choices=PhoneType.choices, default=PhoneType.DIGITAL)
+    department = models.CharField(max_length=255)
+    sponsor = models.CharField(max_length=255, blank=True)    
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Phone"
+        verbose_name_plural = "Phones"
+        ordering = ['department']
+
+    def __str__(self):
+        return f'Ramal: {self.phone_number} | Tipo: {self.phone_type} '
