a9d05b56c8d3b3fc52d715491c34b50eb751d5fe
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-10-12 18:31:27 -0300

    Configured error_message for phone_number

--- phone/forms.py
+++ phone/forms.py
@@ -6,3 +6,9 @@ class PhoneForm(ModelForm):
     class Meta:
         model = Phone
         fields = ['phone_number', 'phone_type', 'department', 'sponsor', 'active']
+
+        error_messages = {
+             "phone_number": {
+                "unique": "Já existe ramal cadastrado com esse número!"
+            }
+        }
